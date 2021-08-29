ab493a51836927f7f59bf251946c2f78cadc1d77
Max Drexel <max-github@drexel.dev>
2021-08-29 02:08:47 -0400

    Contemplating what to do about HashSet; I started creating a wrapper around the underlying HashSet implementation that would expose the new .NET 5 contract, but the underlying implementation doesn't natively support the Serialization interfaces. Should we: * Give up, and just offer an ObjectModel.ReadOnlySet * Meet them half-way and expose everything except the Serialization stuff * Somehow attempt to offer the serialization stuff (probably by copy-pasting the reference source implementation of HashSet?)
    It seems like none of these options are particularly good, because:
    * If we give up, then a user cannot do a simple #ifdef to swap out the HashSet implementation
    * If we meet them half-way, then a user which relies on the Serialization stuff would be broken
    * If we somehow offer the serialization stuff, there's no guarantee that the serialization mechanism will be compatible between the two implementations anyway (unless we guarantee that we will update this library in lock-step with Microsoft, which is not feasible)

---
+++ src/Drexel.Collections.Sets/Generic/IReadOnlySet.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+
+namespace Drexel.Collections.Generic
+{
+    /// <summary>
+    /// Represents a strongly-typed, read-only collection of elements that contains no duplicates. The meaning of
+    /// "duplicate" depends on the implementation of this interface.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the elements.
+    /// </typeparam>
+    public interface IReadOnlySet<T> : IEnumerable<T>, IReadOnlyCollection<T>
+    {
+        /// <summary>
+        /// Determines whether the current set contains the specified element.
+        /// </summary>
+        /// <param name="item">
+        /// The element to locate in the current set.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the item is found in the current set; otherwise, <see langword="false"/>.
+        /// </returns>
+        bool Contains(T item);
+
+        /// <summary>
+        /// Determines whether the current set is a proper subset of the specified <paramref name="other"/>. Being a
+        /// "proper" subset means that <paramref name="other"/> contains all elements of this set, as well as at least
+        /// one element not in this set.
+        /// </summary>
+        /// <param name="other">
+        /// The collection to compare to the current set.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the current set is a proper subset of the specified <paramref name="other"/>;
+        /// otherwise, <see langword="false"/>.
+        /// </returns>
+        bool IsProperSubsetOf(IEnumerable<T> other);
+
+        /// <summary>
+        /// Determines whether the current set is a proper superset of the specified <paramref name="other"/>. Being a
+        /// "proper" superset means that this set contains all the elements of <paramref name="other"/>, as well as at
+        /// least one element not in <paramref name="other"/>.
+        /// </summary>
+        /// <param name="other">
+        /// The collection to compare to the current set.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the current set is a proper superset of the specified <paramref name="other"/>;
+        /// otherwise, <see langword="false"/>.
+        /// </returns>
+        bool IsProperSupersetOf(IEnumerable<T> other);
+
+        /// <summary>
+        /// Determines whether the current set is a subset of the specified <paramref name="other"/>. Being a subset
+        /// means that <paramref name="other"/> contains all elements of this set.
+        /// </summary>
+        /// <param name="other">
+        /// The collection to compare to the current set.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the current set is a subset of the specified <paramref name="other"/>; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        bool IsSubsetOf(IEnumerable<T> other);
+
+        /// <summary>
+        /// Determines whether the current set is a superset of the specified <paramref name="other"/>. Being a
+        /// superset means that this set contains all elements of <paramref name="other"/>.
+        /// </summary>
+        /// <param name="other">
+        /// The collection to compare to the current set.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the current set is a superset of the specified <paramref name="other"/>;
+        /// otherwise, <see langword="false"/>.
+        /// </returns>
+        bool IsSupersetOf(IEnumerable<T> other);
+
+        /// <summary>
+        /// Determines whether the current set overlaps the specified <paramref name="other"/>. Overlapping means that
+        /// this set contains at least one element also contained by <paramref name="other"/>.
+        /// </summary>
+        /// <param name="other">
+        /// The collection to compare to the current set.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the current set overlaps the specified <paramref name="other"/>; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        bool Overlaps(IEnumerable<T> other);
+
+        /// <summary>
+        /// Determines whether the current set is equal to the specified <paramref name="other"/>.
+        /// </summary>
+        /// <param name="other">
+        /// The collection to compare to the current set.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the current set is equal to the specified <paramref name="other"/>; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        bool SetEquals(IEnumerable<T> other);
+    }
+}
